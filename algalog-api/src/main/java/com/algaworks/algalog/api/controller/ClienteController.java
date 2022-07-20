db69a1213a8f2122c134f5fdd0bea3d1550b51e8
igorl <igorllampa@gmail.com>
2022-07-20 17:12:52 -0300

    Add flyway support and generate some scripts. Use Jakarta Persistence with Hibernate implementation for object-relational mapping.

--- algalog-api/src/main/java/com/algaworks/algalog/api/controller/ClienteController.java
+++ algalog-api/src/main/java/com/algaworks/algalog/api/controller/ClienteController.java
@@ -3,6 +3,9 @@ package com.algaworks.algalog.api.controller;
 import java.util.Arrays;
 import java.util.List;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,9 +14,13 @@ import com.algaworks.algalog.domain.model.Cliente;
 @RestController
 public class ClienteController {
 
+	@PersistenceContext
+	private EntityManager manager;
+	
 	@GetMapping("/clientes")
 	public List<Cliente> listar() {
 		
+		/*
 		Cliente cliente1 = new Cliente();
 		cliente1.setId(1L);
 		cliente1.setNome("Tetse");
@@ -26,9 +33,11 @@ public class ClienteController {
 		cliente2.setEmail("email");
 		cliente2.setTelefone("222222222");
 		
+		return Arrays.asList(cliente1, cliente2);
+		*/
 		
+		return manager.createQuery("from Cliente", Cliente.class).getResultList();
 		
-		return Arrays.asList(cliente1, cliente2);     
-		
+		     
 	}
 }
